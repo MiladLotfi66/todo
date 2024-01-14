67ab47d26d69c8ece7c27d2d8fa86e36ba1342b4
miladlotfi <miladlotfi66@gmail.com>
2024-01-14 16:44:41 +0330

    add back and next handler

--- components/module/Tasks.js
+++ components/module/Tasks.js
@@ -1,6 +1,15 @@
 import { RiMastodonLine } from "react-icons/ri";
 import { BiRightArrow, BiLeftArrow } from "react-icons/bi";
 function Tasks({ data, back, next, fetchTodos }) {
+  const changeStatus = async (id, status) => {
+    const res =await fetch("/api/todo", {
+      method: "PATCH",
+      body: JSON.stringify({ id, status }),
+      headers: {"Content-Type": "application/json"}
+    });
+    const data = await res.json();
+    if (data.status === "success") fetchTodos();
+  };
   return (
     <div className="tasks">
       {data?.map((i) => (
@@ -8,9 +17,24 @@ function Tasks({ data, back, next, fetchTodos }) {
           <span className={i.status}></span>
           <RiMastodonLine />
           <h4>{i.title}</h4>
-          <div style={{display:"flex", justifyContent:"space-between"}}>
-          {back ? <button className="button-back"><BiLeftArrow/> back</button> : null}
-          {next ? <button className="button-next">next<BiRightArrow/></button> : null}
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            {back ? (
+              <button
+                className="button-back"
+                onClick={() => changeStatus(i._id, back)}
+              >
+                <BiLeftArrow /> back
+              </button>
+            ) : null}
+            {next ? (
+              <button
+                className="button-next"
+                onClick={() => changeStatus(i._id, next)}
+              >
+                next
+                <BiRightArrow />
+              </button>
+            ) : null}
           </div>
         </div>
       ))}
